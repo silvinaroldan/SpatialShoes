147a186dcc982b9a7ca6e1161e224460f93caf07
Silvina <silvinaroldan@gmail.com>
2024-08-07 10:30:11 -0300

    Initial commit

---
+++ SpatialShoes/SpatialShoes/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SpatialShoes
+//
+//  Created by Silvina Roldan on 07/08/2024.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, Spatial Shoes!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ SpatialShoes/SpatialShoes/SpatialShoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialShoesApp.swift
+//  SpatialShoes
+//
+//  Created by Silvina Roldan on 07/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpatialShoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
