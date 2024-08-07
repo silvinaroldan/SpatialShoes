befd4822b9a18bb6917d442224e70f358fde44cd
Silvina <silvinaroldan@gmail.com>
2024-08-07 11:36:53 -0300

    Add resources and models

---
+++ SpatialShoes/SpatialShoes/Models/Shoe.swift
@@ -0,0 +1,26 @@
+//
+//  Shoe.swift
+//  SpatialShoes
+//
+//  Created by Silvina Roldan on 07/08/2024.
+//
+
+import Foundation
+
+struct Shoe: Codable {
+    let id: Int
+    let name: String
+    let brand: String
+    let size: [Int]
+    let price: Double
+    let description: String
+    let model3DName: String
+    let type: String
+    let materials: [String]
+    let origin: String
+    let gender: String
+    let weight: Double
+    let colors: [String]
+    let warranty: Int
+    let certifications: [String]
+}
