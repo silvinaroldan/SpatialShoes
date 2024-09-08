83f0d8239b3f2770a0254975ce5017a57c09a362
Silvina <silvinaroldan@gmail.com>
2024-09-08 19:36:34 -0300

    Add on tap geture at favorite grid view

--- SpatialShoes/SpatialShoes/Views/ShoeGrid.swift
+++ SpatialShoes/SpatialShoes/Views/ShoeGrid.swift
@@ -10,12 +10,11 @@ import SpatialShoesRC
 import SwiftUI
 
 struct Favorites: View {
-    // TODO: change this variable to environment
     // TODO: check why some shoes are repeated
     // TODO: I think that only one model should rotate and not all at once, maybe we should delete this view
-    
-    
-    @State var shoesVM = ShoesViewModel()
+
+    @Environment(ShoesViewModel.self) private var shoesVM
+    @Environment(\.openWindow) private var open
     @State var rotationAngle: Double = 0.0
 
     // https://www.youtube.com/watch?v=LnPMsG0sV50
@@ -25,27 +24,37 @@ struct Favorites: View {
                 GridRow {
                     ForEach(0..<6) { columnIndex in
                         let index = (rowIndex * 4) + columnIndex
-                        Model3D(named: shoesVM.shoes[index].model3DName, bundle: spatialShoesRCBundle) { model in
-                            model
-                                .resizable()
-                                .scaledToFit()
-                                .scaleEffect(0.5)
-                                //.background(Color.green)
-                                .rotation3DEffect(.degrees(rotationAngle), axis: (x: 0, y: 1, z: 0))
-                        } placeholder: {
-                            ProgressView()
+                        VStack(alignment: .center) {
+                            Model3D(named: shoesVM.shoes[index].model3DName, bundle: spatialShoesRCBundle) { model in
+                                model
+                                    .resizable()
+                                    .scaledToFit()
+                                    .scaleEffect(0.5)
+                                    //.background(Color.green)
+                                    .rotation3DEffect(.degrees(rotationAngle), axis: (x: 0, y: 1, z: 0))
+                                    .onTapGesture {
+                                        shoesVM.selectedShoe = shoesVM.shoes[index]
+                                        // TODO - Goto HomeView()
+                                    }
+
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            
+                            .frame(width: 130, height: 130)
+                            Text(shoesVM.shoes[index].name)
                         }
                     }
                 }
             }
         }
-        .onAppear() {
+        .onAppear {
             doRotation()
         }
     }
-    
+
     func doRotation() {
-        Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true) { timer in
+        Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true) { _ in
             rotationAngle += 0.3
             if rotationAngle >= 360 {
                 rotationAngle = 0
